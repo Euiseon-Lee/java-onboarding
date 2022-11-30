b2dab9fd79fb93b206e278e4b10fecff8b1818d3
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-11-30 11:22:48 +0900

    Problem3 - 369게임

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++) {
+            answer += countClaps(i);
+        }
         return answer;
+    }
+
+    public static int countClaps(int target) {
+        int clap = 0;
+        while(target > 0) {
+            int units = target % 10;
+            if(units == 3 || units == 6 || units == 9) {
+                clap++;
+            }
+            target /= 10;
+        }
+
+        return clap;
     }
 }
